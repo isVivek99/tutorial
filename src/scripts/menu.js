6efdb9d3ce78b8ba86c9ad12f00168455feeead2
vivek lokhande <vivek2811999@gmail.com>
2023-10-08 19:37:31 +0530

    added eventlisteners on view transition and on page mount

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,13 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-  console.log("helol");
-  document.querySelector(".nav-links").classList.toggle("expanded");
-});
+document.addEventListener('astro:page-load', () => {
+  // https://developer.mozilla.org/en-US/docs/Web/API/View_Transitions_API#the_view_transition_process
+  // https://docs.astro.build/en/guides/view-transitions/#astropage-load
+  // since we are using view transitions, 
+  // this event is a part of the flow why navigating
+  // after this event we need to 
+  // mount this event listener again
+  document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.toggle("expanded");
+  });
+
+}, { once: false });
+
